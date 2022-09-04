e5fa0485b9e31ce2ccaad815139b52e95731ce2d
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-09-04 18:43:21 +0300

    making a resized navigation bar that will work on a smaller window

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 let search = document.querySelector('.search-box');
 
-document.querySelector('.search-icon'); onclick = () =>{
+document.querySelector('#search-icon'); onclick = () =>{
     search.classList.toggle('active');
+    // menu.classList.replace('active');
 }
+
+// let menu = document.querySelector('.nav-bar');
+
+// document.querySelector('#menu-icon'); onclick = () =>{
+//     menu.classList.toggle('active');
+//     search.classList.remove('active');
+// }
+
 
 //header 
 
 let header = document.querySelector('header');
 
-window.addEventListener('scroll'), () => {
+window.addEventListener('#scroll'), () => {
     header.classList.toggle('shadow', window.scrollY > 0 );
 
 }
