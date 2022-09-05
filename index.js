09cae5982d305e94ac12efbf9df16daa2258d6be
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-09-05 10:32:11 +0300

    I have added a contact section/form to the website

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-let search = document.querySelector('.search-box');
+// let search = document.querySelector('.search-box');
 
-document.querySelector('#search-icon'); onclick = () =>{
-    search.classList.toggle('active');
-    menu.classList.replace('active');
-}
+// document.querySelector('#search-icon'); onclick = () =>{
+//     search.classList.toggle('active');
+//     menu.classList.replace('active');
+// }
 
-let menu = document.querySelector('.nav-bar');
+// let menu = document.querySelector('.nav-bar');
 
-document.querySelector('#menu-icon'); onclick = () =>{
-    menu.classList.toggle('active');
-    search.classList.remove('active');
-}
+// document.querySelector('#menu-icon'); onclick = () =>{
+//     menu.classList.toggle('active');
+//     search.classList.remove('active');
+// }
+// let header = document.querySelector('header');
 
+// window.addEventListener('#scroll'), () => {
+//     header.classList.toggle('shadow', window.scrollY > 0 );
 
-header 
-
-let header = document.querySelector('header');
-
-window.addEventListener('#scroll'), () => {
-    header.classList.toggle('shadow', window.scrollY > 0 );
-
-}
+// }
 
