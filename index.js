77853a5a9b33566912d319bd7a65bbfa42374b48
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-09-08 00:42:27 +0300

    fetched the API and was able to display the data on the webiste.

--- index.js
+++ index.js
@@ -1,56 +1,16 @@
-function randomQuote(){
-        fetch( "https://quote-garden.herokuapp.com/api/v3/quotes")
-          .then(response => response.json()) 
-          .then(data => {
-            quoteText.textContent = data.content;
-            quoteTags.textContent = data.tags;
-            quoteAuthor.textContent = data.author;
-            })
-        }
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementsByClassName("author");
+const button = document.getElementById("btn");
+function randomQuote() {
+     fetch("https://api.quotable.io/random")
+ .then(response => response.json())
+ .then(data => {
+        quoteText.textContent = data.content;
+        quoteAuthor.textContent = `-- ${data.author}`;
+ }); 
+
+}
+randomQuote();
+button.addEventListener('click', () => {
         randomQuote();
-         quote-btn .addEventListener('click', () => {
-            randomQuote();
-        })
-
-
-
-
-
-        
-// // let carInfo = {
-// //     result: function(model){
-//         fetch("https://quote-garden.herokuapp.com/api/v3/quotes")
-//         .then(res => res.json())
-//         .then(data => console.log(data));
-// //     },
-
-       
-    
-// //     vehicleInfo: function(data){
-// //         const {MakeName} = data.Results[1]
-// //         const {VehicleTypeId} = data.Results[2]
-// //         const {VehicleTypeName} = data.Results[3]
-// //         console.log(VehicleTypeId, VehicleTypeName, MakeName);
-
-
-// //         document.querySelector(".makeName ").innerText = MakeName;
-// //         document.querySelector(".vehicleTypeId ").innerText = VehicleTypeId;
-// //         document.querySelector(".vehicleTypeName ").innerText = VehicleTypeName;
-// //         },
-// //         search: function() {  
-// //                 this.result(document.querySelector(".search-bar").value )
-// //         }
-
-// //     }
-
-// //     document.querySelector(".search-bar").addEventListener("keyup", function(event) {
-// //         if (event.keyCode === 13) {
-
-// //         carInfo.search()
-// // }
-// //     })
-
-
-
-    
-
+});
