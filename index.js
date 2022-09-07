ceeeb30409d6e780926ed896377a6226f532b578
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-09-07 18:35:44 +0300

    started workig on the CSS. The first step was importing font styles from another source

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 
-// let carInfo = {
-//     result: function(model){
-        fetch("https://quote-garden.herokuapp.com/api/v3/quotes")
-        .then(res => res.json())
-        .then(data => console.log(data));
-//     },
+// // let carInfo = {
+// //     result: function(model){
+//         fetch("https://quote-garden.herokuapp.com/api/v3/quotes")
+//         .then(res => res.json())
+//         .then(data => console.log(data));
+// //     },
 
        
     
-//     vehicleInfo: function(data){
-//         const {MakeName} = data.Results[1]
-//         const {VehicleTypeId} = data.Results[2]
-//         const {VehicleTypeName} = data.Results[3]
-//         console.log(VehicleTypeId, VehicleTypeName, MakeName);
+// //     vehicleInfo: function(data){
+// //         const {MakeName} = data.Results[1]
+// //         const {VehicleTypeId} = data.Results[2]
+// //         const {VehicleTypeName} = data.Results[3]
+// //         console.log(VehicleTypeId, VehicleTypeName, MakeName);
 
 
-//         document.querySelector(".makeName ").innerText = MakeName;
-//         document.querySelector(".vehicleTypeId ").innerText = VehicleTypeId;
-//         document.querySelector(".vehicleTypeName ").innerText = VehicleTypeName;
-//         },
-//         search: function() {  
-//                 this.result(document.querySelector(".search-bar").value )
-//         }
+// //         document.querySelector(".makeName ").innerText = MakeName;
+// //         document.querySelector(".vehicleTypeId ").innerText = VehicleTypeId;
+// //         document.querySelector(".vehicleTypeName ").innerText = VehicleTypeName;
+// //         },
+// //         search: function() {  
+// //                 this.result(document.querySelector(".search-bar").value )
+// //         }
 
-//     }
+// //     }
 
-//     document.querySelector(".search-bar").addEventListener("keyup", function(event) {
-//         if (event.keyCode === 13) {
+// //     document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+// //         if (event.keyCode === 13) {
 
-//         carInfo.search()
-// }
-//     })
+// //         carInfo.search()
+// // }
+// //     })
 
 
 
